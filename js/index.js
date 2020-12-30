9e1a1f3ee8fc3790f1fa2ee763149225195b284a
DimaKulikov <dmyukulikov@gmail.com>
2020-12-30 10:53:09 +0300

    feature: render cards from hardcoded array

--- js/index.js
+++ js/index.js
@@ -1,15 +1,65 @@
-// profile DOM elements
+// initial cards
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// profile elements
 let profile = document.querySelector('.profile'),
   popupShowBtn = profile.querySelector('.profile__edit-btn'),
   profileName = profile.querySelector('.profile__name'),
   subtitle = profile.querySelector('.profile__subtitle');
 
-// popup DOM elements
-let popup = document.querySelector('.popup'),
+//list of cards elements
+const cardsList = document.querySelector('.cards__list'),
+  template = document.querySelector('#card-template').content;
+
+// popup elements
+const popup = document.querySelector('.popup'),
   popupCloseBtn = popup.querySelector('.popup__close-btn'),
   profileInputForm = popup.querySelector('form[name=profileEditForm]')
   nameField = popup.querySelector('.popup__input_type_name'),
   subtitleField = popup.querySelector('.popup__input_type_subtitle');
+
+
+// render hardcoded cards on page load
+initialCards.forEach(el => {
+  renderCard(el);
+})
+
+
+// Functions 
+function renderCard(card) {
+  const newCard = template.cloneNode(true);
+  const cardPicture = newCard.querySelector('.card__pic');
+  const cardTitle = newCard.querySelector('.card__title');
+  cardPicture.src = card.link;
+  cardPicture.alt = card.name;
+  cardTitle.textContent = card.name;
+  cardsList.append(newCard)
+}
 
 function showPopup() {
   popup.classList.add('popup_opened'); // show popup block
@@ -39,7 +89,7 @@ function updateProfile(event) {
   closePopup();
 }
 
-
+// Event listeners
 popupShowBtn.addEventListener('click', showPopup);
 popupCloseBtn.addEventListener('click', closePopup);
 profileInputForm.addEventListener('submit', updateProfile);

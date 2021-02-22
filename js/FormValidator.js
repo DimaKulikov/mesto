3ed872dd37ad504743ac5201cb9979b88304c36c
DimaKulikov <dmyukulikov@gmail.com>
2021-02-22 20:39:49 +0300

    begin FormValidator

---
+++ js/FormValidator.js
@@ -0,0 +1,22 @@
+class FormValidator {
+  _form
+  constructor(selectors, formElement) {
+
+  }
+
+  _isValidInput() {
+
+  }
+
+  _toggleButtonState() {
+
+  }
+
+  _setEventListeners() {
+
+  }
+
+  enableValidation() {
+
+  }
+}

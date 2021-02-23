5637ae55dc7a1291ef5d4f6d67b0f03abde66094
DimaKulikov <dmyukulikov@gmail.com>
2021-02-23 09:56:45 +0300

    create FormValidator class

--- js/FormValidator.js
+++ js/FormValidator.js
@@ -1,22 +1,65 @@
 class FormValidator {
-  _form
   constructor(selectors, formElement) {
-
+    this._form = formElement
+    this._selectors = selectors
+  }
+  _showInputError() {
+    this._lastInput.classList.add(this._selectors.inputErrorClass);
+    this._errorContainer.textContent = this._errorMessage;
+    this._errorContainer.classList.add(this._selectors.errorClass);
+  }
+  
+  _hideInputError() {
+    this._lastInput.classList.remove(this._selectors.inputErrorClass);
+    this._errorContainer.classList.remove(this._selectors.errorClass);
+    this._errorContainer.textContent = '';
   }
 
-  _isValidInput() {
-
+  _isValid() {
+    if (!this._lastInput.validity.valid) {
+      this._errorMessage = this._lastInput.validationMessage;
+      this._showInputError()
+    } else {
+      this._hideInputError();
+    }
   }
+
+  _hasInvalidInput() {
+    return this._inputList.some(input => !input.validity.valid);
+  };
 
   _toggleButtonState() {
-
+    if (this._hasInvalidInput(this._inputList)) {
+      this._button.classList.add(this._selectors.inactiveButtonClass);
+    } else {
+      this._button.classList.remove(this._selectors.inactiveButtonClass);
+    }
   }
 
-  _setEventListeners() {
+  _inputChangeHandler(e) {
+    this._lastInput = e.target
+    this._errorContainer = this._form.querySelector(`.${this._lastInput.id}-error`);
+    this._isValid();
+    this._toggleButtonState();
+  }
 
+  _setEventListeners() {    
+    this._inputList.forEach(input => {
+      input.addEventListener('input', (e)=>{
+        this._inputChangeHandler(e)
+      });
+    })
+  }
+
+  _getElements() {
+    this._inputList = Array.from(this._form.querySelectorAll(this._selectors.inputSelector));
+    this._button = this._form.querySelector(this._selectors.submitButtonSelector);
   }
 
   enableValidation() {
-
+    this._getElements()
+    this._setEventListeners()
   }
 }
+
+export {FormValidator}

15b6b9e95694a8ddaebebda53d64b637d9ee104a
Dima <dmyukulokov@gmail.com>
2021-03-08 16:38:28 +0300

    add comments

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,32 @@
+/**
+ * Class representing a section where any content needs to be rendered dynamically
+ */
+
 export default class Section {
-  constructor({data, renderer}, containerSelector){   
+  /**
+   * Creates a section
+   * @param {object} param0 
+   */
+  constructor({data, renderer, containerSelector}){   
     this._container = document.querySelector(containerSelector)
     this.initData = data
     this._renderer = renderer
   }
 
+  /**
+   * Runs every item in the array of initial items through the passed in renderer function
+   */
   renderItems(){
     this.initData.forEach(item => {
       this._renderer(item)
     })
   }
 
+  /**
+   * Inserts an element into the section container
+   * @param {Element} item DOM Node to be inserted into the section container
+   */
   addItem(item){
-    this._container.append(item)    
+    this._container.prepend(item)    
   }
 }

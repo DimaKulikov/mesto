da4e8673eac77509c24296883f445fd89b92a63b
DimaKulikov <dmyukulikov@gmail.com>
2021-03-29 20:57:53 +0300

    passing 'inputElement' as parameter

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -1,61 +1,39 @@
-/**
- * Class enabling live form validation 
- */
-
 export default class FormValidator {
-  /**
-   * Creates a validator instance
-   * @param {object} selectors object containing css selectors for all the necessary elements
-   * @param {*} formElement <form> element to be validated
-   */
   constructor(selectors, formElement) {
     this._form = formElement
     this._selectors = selectors
   }
 
-  /**
-   * Shows an error message corresponding to the input field currently being edited
-   */
-  _showInputError() {
-    this._currentInput.classList.add(this._selectors.inputErrorClass);
-    this._errorContainer.textContent = this._errorMessage;
-    this._errorContainer.classList.add(this._selectors.errorClass);
+  _showInputError(inputElement, errorMessage) {
+    inputElement.classList.add(this._selectors.inputErrorClass);
+    const errorContainer = this._form.querySelector(`.${inputElement.id}-error`);
+    errorContainer.textContent = errorMessage;
+    errorContainer.classList.add(this._selectors.errorClass);
   }
 
-  /**
-   * Hides the error message once the corresponding input field is filled with valid data
-   */  
-  _hideInputError() {
-    this._currentInput.classList.remove(this._selectors.inputErrorClass);
-    this._errorContainer.classList.remove(this._selectors.errorClass);
-    this._errorContainer.textContent = '';
+  _hideInputError(inputElement) {
+    const errorContainer = this._form.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(this._selectors.inputErrorClass);
+    errorContainer.classList.remove(this._selectors.errorClass);
+    errorContainer.textContent = '';
   }
 
-  /**
-   * Checks if the currently edited input is valid. Shows or hides the input error accordingly
-   */
-  _isValid() {
-    if (!this._currentInput.validity.valid) {
-      this._errorMessage = this._currentInput.validationMessage;
-      this._showInputError()
+  _isValid(inputElement) {
+    if (!inputElement.validity.valid) {
+      const errorMessage = inputElement.validationMessage;
+      this._showInputError(inputElement, errorMessage)
     } else {
-      this._hideInputError();
+      this._hideInputError(inputElement);
     }
   }
 
-  /**
-   * Checks all input fields in the form
-   * @returns {boolean} 'true' if any of the input fields is invalid, otherwise 'false'
-   */
   _hasInvalidInput() {
     return this._inputList.some(input => !input.validity.valid);
   };
 
-  /**
-   * Adds or removes 'disabled' attribute and class modifier on the form's submit button 
-   */
+
   _toggleButtonState() {
-    if (this._hasInvalidInput(this._inputList)) {
+    if (this._hasInvalidInput()) {
       this._button.classList.add(this._selectors.inactiveButtonClass);
       this._button.disabled = true;
     } else {
@@ -64,55 +42,35 @@ export default class FormValidator {
     }
   }
 
-  /**
-   * Callback function for input events on any of the input fields
-   * @param {Event} e 
-   */
-  _inputChangeHandler(e) {
-    this._currentInput = e.target
-    this._errorContainer = this._form.querySelector(`.${this._currentInput.id}-error`);
-    this._isValid();
+  _inputChangeHandler(evt) {
+    const input = evt.target
+    this._errorContainer = this._form.querySelector(`.${input.id}-error`);
+    this._isValid(input);
     this._toggleButtonState();
   }
 
-  /**
-   * Callback function for submission of the form
-   * @param {Event} e 
-   */
-  _submitHandler(e) {
-    e.preventDefault();
+  _submitHandler(evt) {
+    evt.preventDefault();
   }
 
-  /**
-   * Sets 'input','submit' and 'reset' event listeners in the process of initialising the validator
-   */
   _setEventListeners() {    
-    this._inputList.forEach(input => {
-      input.addEventListener('input', (e) => { this._inputChangeHandler(e) });
+    this._inputList.forEach(inputElement => {
+      inputElement.addEventListener('input', (e) => { this._inputChangeHandler(e) });
     })
     this._form.addEventListener('submit', (e) => { this._submitHandler(e) });
     this._form.addEventListener('reset', () => {
       this._toggleButtonState();
       this._inputList.forEach((inputElement) => {
-        this._currentInput = inputElement;
-        this._errorContainer = this._form.querySelector(`.${this._currentInput.id}-error`);
-        this._hideInputError()
+        this._hideInputError(inputElement)
       })
     })
-
   }
 
-  /**
-   * Gets form DOM elements and saves them as class instance properties
-   */
   _getElements() {
     this._inputList = Array.from(this._form.querySelectorAll(this._selectors.inputSelector));
     this._button = this._form.querySelector(this._selectors.submitButtonSelector);
   }
 
-  /**
-   * Public method to initialise the validator
-   */
   enableValidation() {
     this._getElements()
     this._setEventListeners()
